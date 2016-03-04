8e7ec374344f85b9325f40d92d7b2da40f56870e
Martin Lissmyr <martin@iMac.local>
2016-03-04 20:41:19 +0100

    Adds calendar view button

--- config/routes.js
+++ config/routes.js
@@ -1,28 +1,11 @@
 var flickrSearch = require("../flickr-search");
 var httpRequest = require("request");
+var trip = require("../trip.json");
 
 // Export the routes and controllers
 module.exports = function(app) {
   app.get("/", function(request, response) {
-    response.render("index", {
-      place: "Hamburg",
-      country: "Germany",
-      participants: [
-        {
-          name: "Slim Jim",
-          avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/kurafire/128.jpg",
-          candidate: true
-        },
-        {
-          name: "Brynn Evans",
-          avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/brynn/128.jpg"
-        },
-        {
-          name: "Henry Lawson",
-          avatar: "https://s3.amazonaws.com/uifaces/faces/twitter/hchicha/128.jpg"
-        }
-      ]
-    });
+    response.render("index", trip);
   });
   app.get("/hero/(*)", function(request, response) {
     flickrSearch(request.params[0], function(url) {
